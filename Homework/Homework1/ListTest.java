90c3ab04219b74bac9000fbbd8fcdc83b85531b5
betolle <54725659+betolle@users.noreply.github.com>
2020-01-22 15:07:46 -0500

    Create ListTest.java

---
+++ Homework/Homework1/ListTest.java
@@ -0,0 +1,68 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ListTest {
+public List<E> A = new EmptyL<E>;
+public List<int> B = new NodeL<int>(4.  A);
+public List<int> C = new NodeL<int>(0.  B);
+
+    @Test
+    public void isEmpty() {
+        assertEquals(true, List.isEmpty(A));
+        assertEquals(false, List.isEmpty(B));
+        assertEquals(false, List.isEmpty(C));
+    }
+
+    @Test
+    public void isSingleton() {
+        assertEquals(false, List.isSingleton(A));
+        assertEquals(true, List.isSingleton(B));
+        assertEquals(false, List.isSingleton(C));
+    }
+
+    @Test
+    public void getFirst() {
+        //expect error assertEquals(, getFirst(A));
+        assertEquals(4, List.getFirst(B));
+        assertEquals(0, List.getFirst(C));
+    }
+
+    @Test
+    public void getRest() {
+        //expect error
+        //expect error
+        assertEquals(4, List.getRest(C));
+
+    }
+
+    @Test
+    public void get() {
+        assertEquals(4, B.get(0));
+        assertEquals(0, C.get(0));
+        assertEquals(4, C.get(1));
+    }
+
+    @Test
+    public void length() {
+        assertEquals(0, A.length());
+        assertEquals(1, B.length());
+        assertEquals(2, C.length());
+
+
+    }
+
+    @Test
+    public void append() {
+    }
+
+    @Test
+    public void contains() {
+        assertEquals(false, A.contains(2));
+        assertEquals(false, B.contains(2));
+        assertEquals(true ,B.contains(4));
+        assertEquals(true ,B.contains(4));
+        assertEquals(false, C.contains(2));
+        assertEquals(true, C.contains(4));
+}
+
