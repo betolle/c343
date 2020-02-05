de7abcb54e340d5ca0dd6f8c2d8d9d5298120287
betolle <54725659+betolle@users.noreply.github.com>
2020-02-05 16:54:35 -0500

    Update LinkedList.java

--- Homework/Homework2/LinkedList.java
+++ Homework/Homework2/LinkedList.java
@@ -2,67 +2,55 @@
 
 class LinkedList<E> implements LinkedListI<E> {
     private List<E> elements;
-    private LinkedList<E> EmptyL;
+
 
     LinkedList () {
         elements = new EmptyL<>();
     }
 
-    private E data;
-    private LinkedList<E> next;
 
-    LinkedList (E data, LinkedList<E> next) {
-        this.data = data;
-        this.next = next;
-    }
 
     public void clear() {
 
-       data = null;
+       elements = new EmptyL<>();
     }
 
     public int size() {
-        if(data == null){
+        if(elements.isEmpty()){
             return   0;
         }
         else {
-
-            return 1 + next.size();
-
+            return 1 + elements.getRest().size();
         }
 
     }
 
     public void addFirst(E elem) {
-
-    new LinkedList<E>(elem, new LinkedList<E>(data, next));
+    elements = new NodeL<>(elem,elements);
 
     }
 
     public void addLast(E elem) {
-
-        new LinkedList<E>(data, new LinkedList (next, new LinkedList(elem, EmptyL));
+         elements.addLast(elem);
 
     }
 
     public E getFirst() throws NoSuchElementE {
-        return data;
+        return elements.getFirst();
     }
 
 
 
-    //need help implementing, need isEmpty?
     public E getLast() throws NoSuchElementE {
-        if(next.isEmpty(); ) {
-return data;
+        return elements.getLast();
+    }
+
+
+    public E removeFirst() throws NoSuchElementE {
+        if (elements.isEmpty()) {
+            throw new NoSuchElementE();
         } else {
-            next.getLast();
+            elements = elements.getRest();
+            return elements.getFirst();
         }
     }
-
-    //return list??
-    public E removeFirst() throws NoSuchElementE {
-
-        return new LinkedList<E>(next.getFirst(), next.removeFirst());
-    }
-}
